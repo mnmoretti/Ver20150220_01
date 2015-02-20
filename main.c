03ce7d5dbf0567df3799e4163b7bbf0f2037741a
mnmoretti <manuel.moretti@tiscali.it>
2015-02-20 09:56:44 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
+    #include <stdio.h>
+    #include <stdlib.h>
 
-int main(int argc, char** argv) {
+    const int N=5;
 
-    return (EXIT_SUCCESS);
-}
+    int main(int argc, char** argv) {
+        int vet[N];
+        int i;
+        int numeroMaggiorenni;
 
+        i=1;
+        while(i<=5) {
+        printf("Inserisci l' eta' della %da persona: ", i);
+        scanf("%d", &vet[i-1]);   
+        i=i+1;
+        }
+        i=0;
+        numeroMaggiorenni=0;
+        while(i<=5) {
+            if(vet[i-1]>17) {
+                numeroMaggiorenni=numeroMaggiorenni+1;
+            }
+            i=i+1;
+        }
+        printf("I maggiorenni sono: %d", numeroMaggiorenni);
+
+        return (numeroMaggiorenni);
+    }
